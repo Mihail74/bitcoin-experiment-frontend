57725b86d29b43470bf673550865e98ff3a54468
Mihail74 <Mihail74@users.noreply.github.com>
2017-12-25 16:53:07 +0500

    Skeleton (#1)
    project skeleton

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import createPersistedState from 'vuex-persistedstate'
+
+import * as modules from './modules'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules,
+  plugins: [
+    createPersistedState({
+      storage: window.localStorage
+    })
+  ]
+})
